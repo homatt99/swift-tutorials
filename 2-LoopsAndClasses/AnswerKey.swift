ae03fc6201362b73e55ebcb0238bf40dcb4b1d14
Matthew Ho <homatt99@yahoo.com>
2021-05-20 18:38:14 -0700

    Exercises for Chiara

--- 2-LoopsAndClasses/AnswerKey.swift
+++ 2-LoopsAndClasses/AnswerKey.swift
@@ -1,12 +1,3 @@
-let name: String = "Matthew"
-let isSunny: Bool = false
-let age: Int = 21
-let weight: Double = 150.5
-var favoriteColor = "Blue"
-
-// name = "John" <- Does not work
-favoriteColor = "Green"
-print(favoriteColor)
 
 /*
 3 parts to a function
@@ -26,25 +17,33 @@ let evenLengthList: [Double] = [1, 4.5, 5, 6] // median = 4.75, mean = 4.125
 let oddLengthList: [Double] = [1, 4.5, 5, 6, 10] // median = 5, mean = 5.3
 let emptyList: [Double] = [] // median = -32, mean = -32
 let unsortedList: [Double] = [3,1, 5, 9, 2,1] // median = 2.5, mean = 3.5
-func getMedian(nums: [Double]) -> Double {
+func getMedian(nums: [Double]) -> Double{
     return 0 //TODO: Implement this
 }
 
-func getMean(nums: [Double]) -> Double {
+func getMean(nums: [Double]) -> Double{
     return 0 //TODO: Implement this
 }
 
 func getSumOfList(nums: [Double]) -> Double {
-    return 0 //TODO: Implement this
+    return 0
 }
 
 func getSmallestNumber(nums: [Double]) -> Double {
-    return 0 //TODO: Implement this
+    let currentSmallest = nums[0]
+    for num in nums {
+        if num < currentSmallest {
+            currentSmallest = num
+        }
+    }
+
+    return currentSmallest
 }
 
 
 
-getSmallestNumber(nums: [1,2,3,4,-1])
+getSmallestNumber(nums: [1,2,3,4, -1])
+
 print(getMedian(nums: evenLengthList))
 print(getMedian(nums: oddLengthList))
 print(getMedian(nums: emptyList))
